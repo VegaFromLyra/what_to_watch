c6431fd466ccc8447ed96942367f5c4b9f27b12f
ashab <asha.ab@gmail.com>
2016-10-27 19:47:06 -0400

    Add string representation for each model

--- suggester/models.py
+++ suggester/models.py
@@ -5,8 +5,14 @@ from django.db import models
 class User(models.Model):
   username = models.CharField(max_length=200)
 
+  def __str__(self):
+    return self.username
+
 class Content(models.Model):
   name = models.CharField
   type = models.CharField(max_length=200)
   user = models.ForeignKey(User)
+
+  def __str__(self):
+      return self.type + ":" + self.name
 
