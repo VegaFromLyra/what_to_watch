7db963851731f475ba9d1cd0f27dddb4d32f72c6
ashab <asha.ab@gmail.com>
2016-12-03 21:10:13 -0500

    Its clickable!!

--- suggester/views.py
+++ suggester/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 def index(request):
-  context = { 'title': 'What do you want to watch?'}
+  context = {
+    'title': 'What do you want to watch?',
+    'message': 'In progress...check back soon!'
+  }
   return render(request, 'suggester/index.html', context)
