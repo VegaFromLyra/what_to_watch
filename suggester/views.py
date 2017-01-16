1695c3491aef364b717b3e7c4e950952a606d839
ashab <asha.ab@gmail.com>
2017-01-15 21:08:13 -0500

    Put title in template + remove unused attribute

--- suggester/views.py
+++ suggester/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 
 def index(request):
   context = {
-    'title': 'What do you want to watch?',
     'message': 'In progress...check back soon!'
   }
   return render(request, 'suggester/index.html', context)
