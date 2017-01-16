6bc58d353e77da4243beca1cc88289583e79f786
ashab <asha.ab@gmail.com>
2017-01-15 20:57:35 -0500

    One way of showing content using different view

--- suggester/urls.py
+++ suggester/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
   url(r'^$', views.index, name='index'),
-  url(r'^suggester/suggest/', views.index, name='index')
+  url(r'^suggest/', views.suggester, name='suggester')
 ]
--- suggester/views.py
+++ suggester/views.py
@@ -6,3 +6,10 @@ def index(request):
     'message': 'In progress...check back soon!'
   }
   return render(request, 'suggester/index.html', context)
+
+def suggester(request):
+  context = {
+    'image_source': 'suggester/mock_content.jpeg'
+  }
+
+  return render(request, 'suggester/content.html', context)
