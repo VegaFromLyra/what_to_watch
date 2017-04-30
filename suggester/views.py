70517e4b644f789914a8fe83fe4b662d11182812
ashab <asha.ab@gmail.com>
2017-04-29 20:27:50 -0400

    Modularize
    - Move http call to MovieDB out of view into a separate content_source
      module
    - Consolidate the poster fetch functionality into a method that returns
      a hash of movie name to poster path vs collection of paths

--- suggester/views.py
+++ suggester/views.py
@@ -1,49 +1,13 @@
 from django.shortcuts import render
-from django.conf import settings
 
 import random
-import requests
-import json
+import content_source
 
 def content(request):
-  payload = {
-    'language': 'en-US',
-    'sort_by': 'popularity.desc'
-  }
-
-  payload.update(api_key())
-
-  response = requests.get(url(), data=payload)
-
-  base = base_image_url()
-
-  posters = []
-
-  for item in response.json()["results"]:
-    posters.append(base + item["poster_path"])
-
+  posters = content_source.posters()
   selected_poster_index = random.randint(0, len(posters) - 1)
-
-  path = posters[selected_poster_index]
+  path = posters.values()[selected_poster_index]
 
   context = { 'image_source': path }
 
   return render(request, 'suggester/content.html', context)
-
-# TODO: Make this private
-def url():
-  return getattr(settings, "MOVIE_DB_URL", None)
-
-def config_url():
-  return getattr(settings, "CONFIG_URL", None)
-
-def base_image_url():
-  response = requests.get(config_url(), data=api_key())
-  parsed_response = response.json()
-  return parsed_response["images"]["secure_base_url"] + image_size() + "/" 
-
-def image_size():
-  return "w500"
-
-def api_key():
-  return { 'api_key': getattr(settings, "MOVIE_DB_KEY", None) }
